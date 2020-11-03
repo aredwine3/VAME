497256c9dc07b5d19074cf91d9788cd2a3943567
Alex Smith <alexander.smith@mssm.edu>
2020-11-03 03:36:54 -0500

    Added workflow

--- vame/custom/helperFunctions.py
+++ vame/custom/helperFunctions.py
@@ -10,6 +10,34 @@ import numpy as np
 import pandas as pd
 import os
 
+def makeEgocentricCSV(h5Path, bodyPart):
+    directory = '/'.join(h5Path.split('/')[:-1])
+    fileName = h5Path.split('/')[-1]#.split('DLC')[0]
+    f, e = os.path.splitext(fileName)
+    df = pd.read_hdf(h5Path)
+    cols = df.columns
+    newCols = cols.droplevel(level=0)
+    df.columns = newCols
+    bodyParts = []
+    for col in newCols:
+        bp = col[0]
+        bodyParts.append(bp)
+    bodyParts = list(set(bodyParts))
+#    for bp in bodyParts:
+#        df.drop(labels=[(bp, 'likelihood')], axis=1, inplace=True)
+    df_ego = df
+    bodyParts_norm = bodyParts
+    bodyParts_norm.remove(bodyPart)
+    for bp in bodyParts_norm:
+        df_ego[(bp, 'x')] = df_ego[(bp, 'x')] - df_ego[(bodyPart, 'x')]
+        df_ego[(bp, 'y')] = df_ego[(bp, 'y')] - df_ego[(bodyPart, 'y')]
+    df_ego[(bodyPart, 'x')] = df_ego[(bodyPart, 'x')] - df_ego[(bodyPart, 'x')] 
+    df_ego[(bodyPart, 'y')] = df_ego[(bodyPart, 'y')] - df_ego[(bodyPart, 'y')]
+    if not os.path.exists(os.path.join(directory, 'egocentric/')):
+        os.mkdir(os.path.join(directory, 'egocentric/'))
+    df_ego.to_csv(os.path.join(directory, 'egocentric/' + f + '_egocentric.csv'))
+    
+    
 def csv_to_numpy(projectPath, csvPath):
     """
     This is a demo function to show how a conversion from the resulting pose-estimation.csv file
@@ -17,11 +45,10 @@ def csv_to_numpy(projectPath, csvPath):
     Note that this code is only useful for data which is a priori egocentric, i.e. head-fixed
     or otherwise restrained animals. 
     """
-    directory = '/'.join(csvPath.split('/')[:-1])
     fileName = csvPath.split('/')[-1].split('DLC')[0]
     f, e = os.path.splitext(fileName)
     # Read in your .csv file, skip the first two rows and create a numpy array
-    data = pd.read_csv(csvPath, skiprows = 2)
+    data = pd.read_csv(csvPath, skiprows = 1)
     data_mat = pd.DataFrame.to_numpy(data)
     data_mat = data_mat[:,1:] 
     
