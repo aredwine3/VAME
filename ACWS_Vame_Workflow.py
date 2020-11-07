c6e0c3b56a8f3acb4f13715a185a2a755f6fce79
Alex Smith <alexander.smith@mssm.edu>
2020-11-07 12:33:32 -0500

    Updated workflow

--- ACWS_Vame_Workflow.py
+++ ACWS_Vame_Workflow.py
@@ -16,14 +16,11 @@ from vame.custom import alignVideos as av
 from vame.analysis.videowriter import motif_videos
 
 
-#Initialize Project:
-directory = '/d1/studies/VAME/VAME_CombinedNPW'
-project = 'VAME_CombinedNPW_NoPaws'
-creationDate = 'Dec7-2020'
-modelName = 'VAME_CombinedNPW3'
-file_format = '.mp4'
 
-videoDirectory = os.path.join(directory, 'mp4s')
+#Initialize Project:
+project = 'VAME_Operant_NoCue'
+directory = '/d1/studies/VAME/VAME_NoCue/'
+videoDirectory = os.path.join(directory, 'videos')
 vids = []
 files = os.listdir(videoDirectory)
 for f in files:
@@ -86,5 +83,68 @@ for n_clusters in clus:
     hf.extractResults(projectPath, group1, group2, modelName, n_clusters, phases)
                      
 hf.extractResults(projectPath, group1, group2, modelName, n_clusters, phases)
+
+from vame.analysis.videowriter import motif_videos
+
+motif_videos(config, model_name='VAME_OperantModel2', cluster_method="kmeans", n_cluster=[10])
+
+samples = os.listdir('/d1/studies/VAME/VAME_OperantModel2/VAME_OperantModel2-Nov4-2020/results/')
+cat = pd.DataFrame()
+for sample in samples:
+    clu_arr = np.load('/d1/studies/VAME/VAME_OperantModel2/VAME_OperantModel2-Nov4-2020/results/' + sample + '/VAME_OperantModel2/kmeans-20/behavior_quantification/motif_usage.npy')
+    clu = pd.DataFrame(clu_arr)
+    clu.columns=[sample]
+    cat = pd.concat([cat, clu], axis=1)
+cat.to_csv('VAME_OperantModel2_10clusters_results.csv')
+
+
+cat.columns
+
+phase1 = pd.DataFrame()
+phase2 = pd.DataFrame()
+phase3 = pd.DataFrame()
+sal = pd.DataFrame()
+acute = pd.DataFrame()
+
+for col in cat.columns:
+    if col.endswith('Phase1'):
+        phase1[col]=cat[col]
+    if col.endswith('Phase2'):
+        phase2[col]=cat[col]
+    if col.endswith('Phase3'):
+        phase3[col]=cat[col]
+    if col.endswith('Saline'):
+        sal[col]=cat[col]
+    if col.endswith('5mgkg'):
+        acute[col]=cat[col]
+
+combined=pd.concat([sal, acute, phase1, phase2, phase3], axis=1)
+
+ctrl_mice = ['C1-RT', 'C3-RB', 'C5-NP', 'C5-RT', 'C9_LT', 'C12_NP', 'C13_RT', 'C14_LT', 'C14_LB', 'C15_RT',]
+cko_mice = ['C2-RB', 'C3-LT', 'C4-NP', 'C4-RT', 'C10_NP', 'C12_RT', 'C13_NP', 'C14_RT', 'C15_NP',]
+ctrl=pd.DataFrame()
+cko = pd.DataFrame()
+
+
+for col in combined.columns:
+    if col[:5] in ctrl_mice:
+        ctrl[col]=combined[col]
+    elif col[:6] in ctrl_mice:
+        ctrl[col]=combined[col]
+    elif col[:5] in cko_mice:
+        cko[col]=combined[col]
+    elif col[:6] in cko_mice:
+        cko[col]=combined[col]
+    else:
+        print(str(col) + " not found in either")
+
+ctrl['control_mean'] = np.mean(ctrl, axis=1)
+ctrl['control_sem']=(np.std(ctrl, axis=1)/np.sqrt((ctrl.shape[1]-1)))
+cko['cko_mean'] = np.mean(cko, axis=1)
+cko['cko_sem']=(np.std(cko, axis=1)/np.sqrt((cko.shape[1]-1)))
+
+comb = pd.concat([ctrl, cko], axis=1)
+comb.to_csv(os.path.join(directory, 'CombinedResults_20Clusters.csv'))
+
 
 
