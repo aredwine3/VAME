dd47d9e04d41721b08e4c6bd3662ccf9113d3041
Alex Smith <alexander.smith@mssm.edu>
2020-11-07 12:32:07 -0500

    added comments

--- vame/custom/helperFunctions.py
+++ vame/custom/helperFunctions.py
@@ -20,7 +20,7 @@ def makeEgocentricCSV(h5Path, bodyPart):
     f, e = os.path.splitext(fileName)
     df = pd.read_hdf(h5Path)
     cols = df.columns
-    newCols = cols.droplevel(level=0) #drops the 'scorer' level from the h5 MultiIndex
+    newCols = cols.droplevel(level=0) #drops the 'scorer' level from the DLC data MultiIndex
     df.columns = newCols
     bodyParts = []
     for col in newCols:
@@ -31,10 +31,10 @@ def makeEgocentricCSV(h5Path, bodyPart):
     bodyParts_norm = bodyParts
     bodyParts_norm.remove(bodyPart)
     for bp in bodyParts_norm: #normalize bodyparts by subtracting one from all 
-        df_ego[(bp, 'x')] = df_ego[(bp, 'x')] - df_ego[(bodyPart, 'x')] #for x position
-        df_ego[(bp, 'y')] = df_ego[(bp, 'y')] - df_ego[(bodyPart, 'y')] #for y position
-    df_ego[(bodyPart, 'x')] = df_ego[(bodyPart, 'x')] - df_ego[(bodyPart, 'x')]  
-    df_ego[(bodyPart, 'y')] = df_ego[(bodyPart, 'y')] - df_ego[(bodyPart, 'y')]  
+        df_ego[(bp, 'x')] = df_ego[(bp, 'x')] - df_ego[(bodyPart, 'x')]
+        df_ego[(bp, 'y')] = df_ego[(bp, 'y')] - df_ego[(bodyPart, 'y')]
+    df_ego[(bodyPart, 'x')] = df_ego[(bodyPart, 'x')] - df_ego[(bodyPart, 'x')] 
+    df_ego[(bodyPart, 'y')] = df_ego[(bodyPart, 'y')] - df_ego[(bodyPart, 'y')]
     if not os.path.exists(os.path.join(directory, 'egocentric/')):
         os.mkdir(os.path.join(directory, 'egocentric/'))
     df_ego.to_csv(os.path.join(directory, 'egocentric/' + f + '_egocentric.csv'))
@@ -67,37 +67,8 @@ def makeEgocentricCSV_Center(h5Path, bodyPart1, bodyPart2, drop=None):
         os.mkdir(os.path.join(directory, 'egocentric/'))
     df_ego.to_csv(os.path.join(directory, 'egocentric/' + f + '_egocentric_centered.csv'))
 
-
-def makeEgocentricCSV(h5Path, bodyPart):
-    directory = '/'.join(h5Path.split('/')[:-1])
-    fileName = h5Path.split('/')[-1]#.split('DLC')[0]
-    f, e = os.path.splitext(fileName)
-    df = pd.read_hdf(h5Path)
-    cols = df.columns
-    newCols = cols.droplevel(level=0)
-    df.columns = newCols
-    bodyParts = []
-    for col in newCols:
-        bp = col[0]
-        bodyParts.append(bp)
-    bodyParts = list(set(bodyParts))
-#    for bp in bodyParts:
-#        df.drop(labels=[(bp, 'likelihood')], axis=1, inplace=True)
-    df_ego = df
-    bodyParts_norm = bodyParts
-    bodyParts_norm.remove(bodyPart)
-    for bp in bodyParts_norm:
-        df_ego[(bp, 'x')] = df_ego[(bp, 'x')] - df_ego[(bodyPart, 'x')]
-        df_ego[(bp, 'y')] = df_ego[(bp, 'y')] - df_ego[(bodyPart, 'y')]
-    df_ego[(bodyPart, 'x')] = df_ego[(bodyPart, 'x')] - df_ego[(bodyPart, 'x')] 
-    df_ego[(bodyPart, 'y')] = df_ego[(bodyPart, 'y')] - df_ego[(bodyPart, 'y')]
-    if not os.path.exists(os.path.join(directory, 'egocentric/')):
-        os.mkdir(os.path.join(directory, 'egocentric/'))
-    df_ego.to_csv(os.path.join(directory, 'egocentric/' + f + '_egocentric.csv'))
     
-    
-def csv_to_numpy(projectPath, csvPath):
-
+def csv_to_numpy(projectPath, csvPath, pcutoff=.99):
     """
     This is a demo function to show how a conversion from the resulting pose-estimation.csv file
     to a numpy array can be implemented. 
@@ -197,10 +168,64 @@ def combineBehavior(config, save=True, n_cluster=30):
         df2.to_csv(os.path.join(project_path, 'results/Motif_Usage_Combined_' + str(n_cluster) + 'clusters.csv'))
     return(df2)
         seq[con_arr[idx,:]<.99] = np.NaN
-        body_position_nan.append(seq)
     
     final_positions = np.array(body_position_nan)
-    
     # save the final_positions array with np.save()
     np.save(os.path.join(projectPath, 'data/' + f + '/' + f + "-PE-seq.npy"), final_positions)
+
+def combineBehavior(config, save=True, n_cluster=30):
+    config_file = Path(config).resolve()
+    cfg = read_config(config_file)
+    project_path = cfg['project_path']
+    files = []
+    if cfg['all_data'] == 'No':
+        all_flag = input("Do you want to write motif videos for your entire dataset? \n"
+                     "If you only want to use a specific dataset type filename: \n"
+                     "yes/no/filename ")
+    else: 
+        all_flag = 'yes'
+        
+    if all_flag == 'yes' or all_flag == 'Yes':
+        for file in cfg['video_sets']:
+            files.append(file)
+            
+    elif all_flag == 'no' or all_flag == 'No':
+        for file in cfg['video_sets']:
+            use_file = input("Do you want to quantify " + file + "? yes/no: ")
+            if use_file == 'yes':
+                files.append(file)
+            if use_file == 'no':
+                continue
+    else:
+        files.append(all_flag)
+    
+    cat = pd.DataFrame()
+    for file in files:
+        arr = np.load(os.path.join(project_path, 'results/' + file + '/VAME_NPW/kmeans-' + str(n_cluster) + '/behavior_quantification/motif_usage.npy'))
+        df = pd.DataFrame(arr, columns=[file])
+        cat = pd.concat([cat, df], axis=1)
+
+    phases=[]
+    mice=[]
+    for col in cat.columns:
+        phase = col.split('_')[1]
+        phases.append(phase)
+        mouse = col.split('_')[0]
+        mice.append(mouse)
+    lz = list(zip(mice, phases))    
+    lz = sorted(lz)
+    joined = []
+    for pair in lz:
+        j = '_'.join(pair)
+        joined.append(j)
+    ind = sorted(lz)
+    ind = pd.MultiIndex.from_tuples(ind)
+    df2 = pd.DataFrame()
+    for sample in joined:
+        df2[sample] = cat[sample]
+    df2.columns=ind
+    
+    if save:
+        df2.to_csv(os.path.join(project_path, 'results/Motif_Usage_Combined_' + str(n_cluster) + 'clusters.csv'))
+    return(df2)
 
