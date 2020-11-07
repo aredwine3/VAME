5f9871c4f2868e19b416c21cc80bc6a847a39491
Alex Smith <alexander.smith@mssm.edu>
2020-11-07 12:32:07 -0500

    added comments

--- vame/custom/helperFunctions.py
+++ vame/custom/helperFunctions.py
@@ -32,6 +32,7 @@ def makeEgocentricCSV(h5Path, bodyPart):
     for bp in bodyParts_norm: #normalize bodyparts by subtracting one from all 
         df_ego[(scorerName, bp, 'x')] = df_ego[(scorerName, bp, 'x')] - df_ego[(scorerName, bodyPart, 'x')] #for x position
         df_ego[(scorerName, bp, 'y')] = df_ego[(scorerName, bp, 'y')] - df_ego[(scorerName, bodyPart, 'y')] #for y position
+
     if not os.path.exists(os.path.join(directory, 'egocentric/')):
         os.mkdir(os.path.join(directory, 'egocentric/'))
     df_ego.to_csv(os.path.join(directory, 'egocentric/' + f + '_egocentric.csv'))
@@ -64,37 +65,8 @@ def makeEgocentricCSV_Center(h5Path, bodyPart1, bodyPart2, drop=None):
         os.mkdir(os.path.join(directory, 'egocentric/'))
     df_ego.to_csv(os.path.join(directory, 'egocentric/' + f + '_egocentric_centered.csv'))
 
-
-def makeEgocentricCSV(h5Path, bodyPart):
-    directory = '/'.join(h5Path.split('/')[:-1])
-    fileName = h5Path.split('/')[-1]#.split('DLC')[0]
-    f, e = os.path.splitext(fileName)
-    df = pd.read_hdf(h5Path)
-    cols = df.columns
-    newCols = cols.droplevel(level=0)
-    df.columns = newCols
-    bodyParts = []
-    for col in newCols:
-        bp = col[0]
-        bodyParts.append(bp)
-    bodyParts = list(set(bodyParts))
-#    for bp in bodyParts:
-#        df.drop(labels=[(bp, 'likelihood')], axis=1, inplace=True)
-    df_ego = df
-    bodyParts_norm = bodyParts
-    bodyParts_norm.remove(bodyPart)
-    for bp in bodyParts_norm:
-        df_ego[(bp, 'x')] = df_ego[(bp, 'x')] - df_ego[(bodyPart, 'x')]
-        df_ego[(bp, 'y')] = df_ego[(bp, 'y')] - df_ego[(bodyPart, 'y')]
-    df_ego[(bodyPart, 'x')] = df_ego[(bodyPart, 'x')] - df_ego[(bodyPart, 'x')] 
-    df_ego[(bodyPart, 'y')] = df_ego[(bodyPart, 'y')] - df_ego[(bodyPart, 'y')]
-    if not os.path.exists(os.path.join(directory, 'egocentric/')):
-        os.mkdir(os.path.join(directory, 'egocentric/'))
-    df_ego.to_csv(os.path.join(directory, 'egocentric/' + f + '_egocentric.csv'))
     
-    
-def csv_to_numpy(projectPath, csvPath):
-
+def csv_to_numpy(projectPath, csvPath, pcutoff=.99):
     """
     This is a demo function to show how a conversion from the resulting pose-estimation.csv file
     to a numpy array can be implemented. 
@@ -194,10 +166,8 @@ def combineBehavior(config, save=True, n_cluster=30):
         df2.to_csv(os.path.join(project_path, 'results/Motif_Usage_Combined_' + str(n_cluster) + 'clusters.csv'))
     return(df2)
         seq[con_arr[idx,:]<.99] = np.NaN
-        body_position_nan.append(seq)
     
     final_positions = np.array(body_position_nan)
-    
     # save the final_positions array with np.save()
     np.save(os.path.join(projectPath, 'data/' + f + '/' + f + "-PE-seq.npy"), final_positions)
 
@@ -455,6 +425,7 @@ def dropBodyParts(projectPath, bodyParts):
     final_positions = np.array(body_position_nan)
     # save the final_positions array with np.save()
     np.save(os.path.join(projectPath, 'data/' + f + '/' + f + "-PE-seq.npy"), final_positions)
+
 
 def combineBehavior(config, save=True, n_cluster=30):
     config_file = Path(config).resolve()
