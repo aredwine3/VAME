d4a373954c04eab44d72da38b79fe24651a9f15f
Alex Smith <alexander.smith@mssm.edu>
2020-11-24 10:34:55 -0500

    Updated workflow

--- ACWS_Vame_Workflow.py
+++ ACWS_Vame_Workflow.py
@@ -15,12 +15,14 @@ from vame.custom import helperFunctions as hf
 from vame.custom import alignVideos as av
 
 
+
 new = False #Set to True to create new project, False to load config file
 
 #Initialize Project:
 project = 'VAME_CombinedNPW3'
 directory = '/d1/studies/VAME/VAME_CombinedNPW'
 modelName = 'VAME_CombinedNPW3'
+
 videoDirectory = os.path.join(directory, 'mp4s')
 vids = []
 files = os.listdir(videoDirectory)
@@ -70,6 +72,9 @@ vame.evaluate_model(config, model_name=modelName)
 vame.behavior_segmentation(config, model_name=modelName, cluster_method='kmeans', n_cluster=[10,20,30])
 #Quantify behaviors:
 vame.behavior_quantification(config, model_name=modelName, cluster_method='kmeans', n_cluster=30)
+vame.behavior_segmentation(config, model_name=modelName, cluster_method='kmeans', n_cluster=[10,20,30,40])
+#Quantify behaviors:
+vame.behavior_quantification(config, model_name=modelName, cluster_method='kmeans', n_cluster=10)
 
 from vame.analysis.videowriter import motif_videos
 
@@ -149,4 +154,5 @@ cko_p2.to_csv(os.path.join(directory, 'cKO_NewAlignment_Phase2_30clusters.csv'))
 
 results = pd.concat([ctrl_p1, cko_p1], axis=1)
 results.to_csv(os.path.join(directory, 'Combined_NewAlignment_Phase2_30clusters_Results.csv'))
+
 
