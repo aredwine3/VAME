3ff92b1c5a65eeaa4d3668b0a86c32717fc40321
Alex Smith <smitaw@musc.edu>
2022-02-10 10:21:00 -0500

    Updated main workflow

--- ACWS_VAME_Updated_RunScript.py
+++ ACWS_VAME_Updated_RunScript.py
@@ -9,35 +9,53 @@ https://github.com/LINCellularNeuroscience/VAME
 Licensed under GNU General Public License v3.0
 """
 
-import vame
+import matplotlib
+matplotlib.use('Qt5Agg')
+import matplotlib.pyplot as plt
 
-# These paths have to be set manually 
-working_directory = '/d1/studies/VAME'
-project='OptoSideProfile'
-videos = ['/d1/studies/VAME/VAME_20FPS_SIDEVIEWMODEL/videos/']
+import os
+os.chdir('/d1/software/VAME')
+import vame
+import numpy as np
+import glob
+from vame.util import auxiliary as aux
+from vame.analysis import behavior_structure as bs
+from vame.custom import helperFunctions as hf
+from vame.custom import ACWS_videowriter as avw
+
+
+new = True #set to True if you're creating a new project, leave False if resuming an old one.
     
 # Initialize your project
 # Step 1.1:
-config = vame.init_new_project(project=project, videos=videos, working_directory=working_directory, videotype='.mp4')
+if new:
+    working_directory = '/d1/studies/DLC_Data/MouseIVSA_VAME/'
+    project='SideProfileRTA'
+    videos = ['/d1/studies/DLC_Data/vGluT2_RTA-smith-2022-01-18/videos/']
+    config = vame.init_new_project(project=project, videos=videos, working_directory=working_directory, videotype='.mp4')
+elif not new:
+    config = '/d1/studies/DLC_Data/MouseIVSA_VAME/MouseIVSA_SmithLabVAME-Dec22-2021/config.yaml'
+working_directory=os.path.dirname(config)
+os.chdir(working_directory)
 
-# After the inital creation of your project you can always access the config.yaml file 
-# via specifying the path to your project
-config = '/YOUR/WORKING/DIRECTORY/Your-VAME-Project-Apr14-2020/config.yaml'
+cfg=aux.read_config(config)
+n_cluster=cfg['n_cluster']
+cluster_method='kmeans'
+projectPath = cfg['project_path']
+modelName = cfg['model_name']
+vids = cfg['video_sets']
 
-# As our config.yaml is sometimes still changing a little due to updates, we have here a small function
-# to update your config.yaml to the current state. Be aware that this will overwrite your current config.yaml
-# and make sure to back up your version if you did parameter changes!
-vame.update_config(config)
+#Note there is a vame.update_config(config) function that should be run after updating vame version.
+
+#Optionally drop 'bodyparts' if they are stationary objects that should not be considered part of 'pose':
+hf.dropBodyParts(config, ['barrier', 'LED'])
 
 # Step 1.2:
 # Align your behavior videos egocentric and create training dataset:
 # pose_ref_index: list of reference coordinate indices for alignment
 # Example: 0: snout, 1: forehand_left, 2: forehand_right, 3: hindleft, 4: hindright, 5: tail
-vame.egocentric_alignment(config, pose_ref_index=[0,1,2,3,4,5,6,7,8])
-
-# If your experiment is by design egocentrical (e.g. head-fixed experiment on treadmill etc) 
-# you can use the following to convert your .csv to a .npy array, ready to train vame on it
-#vame.csv_to_numpy(config, datapath='C:\\Research\\VAME\\vame_alpha_release-Mar16-2021\\videos\\pose_estimation\\')
+# If you want to view the result, set check_video to True. If you want to save the result, set use_video, check_video, and save all to True.
+vame.egocentric_alignment(config, crop_size=(400,400), pose_ref_index=[0,8], use_video=False, check_video=False, save=False)
 
 # Step 1.3:
 # create the training set for the VAME model
@@ -49,7 +67,8 @@ vame.train_model(config)
 
 # Step 3:
 # Evaluate model
-vame.evaluate_model(config)
+vame.evaluate_model(config, model_name=modelName, use_snapshots=True)
+hf.plotLoss(config)
 
 # Step 4:
 # Segment motifs/pose
@@ -61,7 +80,27 @@ vame.pose_segmentation(config)
 # community videos
 
 # OPTIONIAL: Create motif videos to get insights about the fine grained poses
-vame.motif_videos(config, videoType='.mp4')
+vame.motif_videos(config, model_name='VAME', videoType='.mp4', fps=30)
+
+#Rather than the above, this line will make motif videos that contain the 
+avw.motif_videos(config, model_name=modelName, videoType='.mp4', fps=30)
+
+#Generate transition matrices & average transition matrixes for groups:
+g1=glob.glob(os.path.join(projectPath, 'results', 'Cue*'))
+group1=[]
+g2=glob.glob(os.path.join(projectPath, 'results', 'Ext*'))
+group2=[]
+for g in g1:
+    fname = g.split('/')[-1]
+    group1.append(fname)
+for g in g2:
+    fname = g.split('/')[-1]
+    group2.append(fname)
+
+hf.plotAverageTransitionMatrices(config, group1, group2=None, g1name='IVSA', g2name=None, cluster_method='kmeans')
+
+hf.combineBehavior(config, save=True, cluster_method='kmeans', legacy=False)
+hf.parseIVSA(config, groups=['IVSA', 'Ext', 'Reinstatement', 'Cue'])
 
 # OPTIONAL: Create behavioural hierarchies via community detection
 vame.community(config, show_umap=False, cut_tree=2)
@@ -70,17 +109,25 @@ vame.community(config, show_umap=False, cut_tree=2)
 vame.community_videos(config)
 
 # OPTIONAL: Down projection of latent vectors and visualization via UMAP
-vame.visualization(config, label=None) #options: label: None, "motif", "community"
+vame.visualization(config, label='motif') #options: label: None, "motif", "community"
 
 # OPTIONAL: Use the generative model (reconstruction decoder) to sample from 
 # the learned data distribution, reconstruct random real samples or visualize
 # the cluster center for validation
-vame.generative_model(config, mode="centers") #options: mode: "sampling", "reconstruction", "centers", "motifs"
+vame.generative_model(config, mode="motifs") #options: mode: "sampling", "reconstruction", "centers", "motifs"
 
 # OPTIONAL: Create a video of an egocentrically aligned mouse + path through 
 # the community space (similar to our gif on github) to learn more about your representation
 # and have something cool to show around ;) 
 # Note: This function is currently very slow. Once the frames are saved you can create a video
 # or gif via e.g. ImageJ or other tools
-vame.gif(config, pose_ref_index=[0,5], subtract_background=True, start=None, 
-         length=500, max_lag=30, label='community', file_format='.mp4', crop_size=(300,300))
+vame.gif(config, pose_ref_index=[0,5], subtract_background=False, start=None, 
+         length=500, max_lag=30, label='motif', file_format='.mp4', crop_size=(350,350))
+
+
+files=["/d1/studies/DLC_Data/MouseIVSA_VAME/MouseIVSA_SmithLabVAME-Dec22-2021/IVSA_MotifUsage.csv",
+"/d1/studies/DLC_Data/MouseIVSA_VAME/MouseIVSA_SmithLabVAME-Dec22-2021/Ext_MotifUsage.csv",
+"/d1/studies/DLC_Data/MouseIVSA_VAME/MouseIVSA_SmithLabVAME-Dec22-2021/Cue_MotifUsage.csv"]
+
+
+combineMotifUsage(config, files)
