6ce06040e2bcc91d4e950ccb052c3d5f5a2645c7
Alex Smith <smitaw@musc.edu>
2023-08-29 09:56:12 -0400

    Fixed bug where zeros in first clusters of motif_usage.npy were not detected and corrected

--- vame/analysis/pose_segmentation.py
+++ vame/analysis/pose_segmentation.py
@@ -123,6 +123,12 @@ def consecutive(data, stepsize=1):
 
 def get_motif_usage(label, n_cluster):
     motif_usage = np.unique(label, return_counts=True)
+    motif_usage=list(motif_usage)
+    for i in range(n_cluster):
+        if motif_usage[0][i]!=i:
+            motif_usage[0] = np.insert(motif_usage[0], i, i, axis=0)
+            motif_usage[1] = np.insert(motif_usage[1], i, 0, axis=0)
+    motif_usage = tuple(motif_usage)
     cons = consecutive(motif_usage[0])
     if len(cons) != 1:
         used_motifs = list(motif_usage[0])
@@ -161,11 +167,11 @@ def same_parameterization(cfg, files, latent_vector_files, states, parameterizat
             hmm_model.fit(latent_vector_cat)
             label = hmm_model.predict(latent_vector_cat)
             save_data = os.path.join(cfg['project_path'], "results")
-            with open(save_data+"hmm_trained_ncluster"+str(states)+".pkl", "wb") as file: pickle.dump(hmm_model, file)
+            with open(os.path.join(save_data+"hmm_trained_ncluster"+str(states)+".pkl"), "wb") as file: pickle.dump(hmm_model, file)
         else:
             print("Using a pretrained HMM as parameterization!")
-            save_data = os.path.join(cfg['project_path'], "results", "")
-            with open(save_data+"hmm_trained_ncluster"+str(states)+".pkl", "rb") as file:
+            save_data = os.path.join(cfg['project_path'], "results")
+            with open(os.path.join(save_data, "hmm_trained_ncluster"+str(states)+".pkl"), "rb") as file:
                 hmm_model = pickle.load(file)
             label = hmm_model.predict(latent_vector_cat)
         
